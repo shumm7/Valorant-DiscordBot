6c7d3a4b248456fcf14eb522b577b0d865014dd8
しゅう <44467870+shumm7@users.noreply.github.com>
2022-08-22 21:39:27 +0900

    1.1.3
    /crosshairコマンドで使用するデータの保管場所を変更
    言語ファイルの配置を変更
    /weaponコマンドを追加
    /weaponコマンドで使用する絵文字を3種類追加
    キャッシュの取得方法を修正
    config.jsonを追加（今後使用予定）
    /bundlesコマンドを廃止し、/featureコマンドに変更
    コマンドの説明文の修正

--- utils/valorant/local.py
+++ utils/valorant/local.py
@@ -13,20 +13,7 @@ from typing import Any, Dict
 Locale = {
     'en-US': 'en-US',  # american_english
     'en-GB': 'en-US',  # british_english
-    'zh-CN': 'zh-CN',  # chinese
-    'zh-TW': 'zh-TW',  # taiwan_chinese
-    'fr': 'fr-FR',  # french
-    'de': 'de-DE',  # german
-    'it': 'it-IT',  # italian
     'ja': 'ja-JP',  # japanese
-    'ko': 'ko-KR',  # korean
-    'pl': 'pl-PL',  # polish
-    'pt-BR': 'pt-BR',  # portuguese_brazil
-    'ru': 'ru-RU',  # russian
-    'es-ES': 'es-ES',  # spanish
-    'th': 'th-TH',  # thai
-    'tr': 'tr-TR',  # turkish
-    'vi': 'vi-VN',  # vietnamese
 }
 
 
@@ -37,7 +24,7 @@ def InteractionLanguage(local_code: str) -> Dict[str, Any]:
 def __LocalRead(filename: str) -> Dict:
     data = {}
     try:
-        with open(f"languages/{filename}.json", "r", encoding='utf-8') as json_file:
+        with open(f"lang/{filename}.json", "r", encoding='utf-8') as json_file:
             data = json.load(json_file)
     except FileNotFoundError:
         return __LocalRead('en-US')
@@ -47,10 +34,16 @@ def __LocalRead(filename: str) -> Dict:
 def ResponseLanguage(command_name: str, local_code: str) -> Dict[str, Any]:
     local_code = __verify_localcode(local_code)
     local = {}
-    with contextlib.suppress(KeyError):
-        local_dict = __LocalRead(local_code)
-        local = local_dict['commands'][str(command_name)]
-    return local
+    if command_name==None or len(command_name)==0:
+        with contextlib.suppress(KeyError):
+            local_dict = __LocalRead(local_code)
+            local = local_dict['commands']
+        return local
+    else:
+        with contextlib.suppress(KeyError):
+            local_dict = __LocalRead(local_code)
+            local = local_dict['commands'][str(command_name)]
+        return local
 
 
 def LocalErrorResponse(value: str, local_code: str) -> Dict[str, Any]:
